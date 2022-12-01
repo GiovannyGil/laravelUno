b6fcb3c58807363e8656b2896b800f4604d96520
GiovannyGil <102708414+GiovannyGil@users.noreply.github.com>
2022-11-30 22:27:11 -0500

    Up1 v1

--- resources/views/livewire/games.blade.php
+++ resources/views/livewire/games.blade.php
@@ -1,34 +1,38 @@
-
-
-<button wire:click="crear()" class="bg-green-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded">Agregar</button> 
-
-@if($modal)
-  @include("livewire.AgregarG()")
-@endif
-<div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
-    <table class="min-w-full border-collapse block md:table">
-        <thead class="block md:table-header-group">
-        <tr class="border border-grey-500 md:border-none block md:table-row absolute -top-full md:top-auto -left-full md:left-auto  md:relative ">
-                <th class="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">id</th>
-				<th class="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">nombre</th>
-				<th class="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">genero</th>
-				<th class="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">categoria</th>
-				<th class="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Actions</th>
+<x-slot name="header">
+    <center><h1 class="text-gray-900">GESTION DE JUEGOS</h1></center> 
+ </x-slot>    
+ <div class="py-12">
+     <div class="max-w-7x1 mx-auto sm:px6 lg:px-8">
+         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-4">
+ 
+             <button wire:click="crear()" class="bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 my3 rounded-md " >ADD</button><br>
+             
+             @if($modal)
+                 @include('livewire.crearG')
+             @endif    
+             <br>
+ 
+ <table  class="table-fixed w-full">
+     <thead>
+         <tr class="bg-blue-600 text-white">
+                <th class="px-4 py-2">id</th>
+				<th class="px-4 py-2">nombre</th>
+				<th class="px-4 py-2">genero</th>
+				<th class="px-4 py-2">categoria</th>
+				<th class="px-4 py-2">Actions</th>
             </tr>
         </thead>
         <tbody class="block md:table-row-group">
             @foreach($games as $game)
             
-            <tr class="bg-gray-300 border border-grey-500 md:border-none block md:table-row">
-                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">id</span>{{$game->id}}</td>
-                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">nombre</span>{{$game->Nombre}}</td>
-                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">genero</span>{{$game->Genero}}</td>
-                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">categoria</span>{{$game->Categoria}}</td>
-                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">
-					<span class="inline-block w-1/3 md:hidden font-bold">Actions</span>
-					<button wire:click="editar({{$game->id}})" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded">Edit</button>
-					<button wire:click="borrar({{$game->id}})" class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-500 rounded">Delete</button>
+            <tr>
+                <td class="border px-4 py2"><span class="inline-block w-1/3 md:hidden font-bold">id</span>{{$game->id}}</td>
+                <td class="border px-4 py2"><span class="inline-block w-1/3 md:hidden font-bold">nombre</span>{{$game->Nombre}}</td>
+                <td class="border px-4 py2"><span class="inline-block w-1/3 md:hidden font-bold">genero</span>{{$game->Genero}}</td>
+                <td class="border px-4 py2"><span class="inline-block w-1/3 md:hidden font-bold">categoria</span>{{$game->Categoria}}</td>
+                <td class="border px-4 py-2 text-center">
+                    <button wire:click="editar({{$game->id}})"class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4  my3 rounded-md">Edit</button>
+                    <button wire:click="borrar({{$game->id}})"class="bg-red-700 hover:bg-red-800 text-white font-bold py-2 px-4  my3 rounded-md">Delete</button>
                 </td>
             </tr>
             @endforeach
